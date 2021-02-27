c53189aa1c30705fd7e08a202a01e5e9477740bf
Misha_Badichel <karandaw9@gmail.com>
2021-02-27 18:28:21 +0200

    User class added.

---
+++ yoptaKursach/Properties/User/User.cs
@@ -0,0 +1,35 @@
+﻿namespace yoptaKursach.Properties.User
+{
+    public enum UserRole
+    {
+        Administrator,
+        Manager,
+        Consumer
+    }
+
+    public class User
+    {
+        public User()
+        {
+            //TODO: Make a query to DB
+        }
+
+        public int Id { get; }
+
+        public UserRole Role { get; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string DocumentNumber { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Sex { get; set; }
+    }
+}
